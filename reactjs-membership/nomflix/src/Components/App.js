dff9e0420bd9f782c6c4633f3841500f1d8b2a24
loegnah <loegnah@github.com>
2021-08-19 18:58:48 +0900

    nomflix 3.4

--- reactjs-membership/nomflix/src/Components/App.js
+++ reactjs-membership/nomflix/src/Components/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Router from "Components/Router";
-import Header from "Components/Header";
+import GlobalStyles from "./GlobalStyles";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <Router />
-      </>
-    );
-  }
+function App()  {
+  return (
+    <>
+      <Router />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
