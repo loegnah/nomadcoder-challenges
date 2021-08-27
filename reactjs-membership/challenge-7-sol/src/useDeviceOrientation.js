2e82f6eb3b781a4a09de2662583422c0a0339068
loegnah <loegnah@github.com>
2021-08-27 20:55:41 +0900

    reactjs-mb challenge 7 solution

---
+++ reactjs-membership/challenge-7-sol/src/useDeviceOrientation.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export function useDeviceOrientation(onOrientationChange) {
+  const [coords, setCoords] = useState({
+    alpha: "null",
+    beta: "null",
+    gamma: "null"
+  });
+  const onMovement = (event) => {
+    const { alpha, beta, gamma } = event;
+    setCoords({ alpha, beta, gamma });
+    if (typeof onOrientationChange === "function") {
+      onOrientationChange(event);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("deviceorientation", onMovement);
+    return () => window.removeEventListener("deviceorientation", onMovement);
+  }, []);
+  return coords;
+}
